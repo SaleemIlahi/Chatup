5cc603f90836c1804157ab74b65398730654f7d5
Mohamed Saleem <saleemilahi21@gmail.com>
2022-07-29 16:49:12 +0530

    updated with DB connection and controllers - routers

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const userRouter = require("./api/routes/userRouter.js");
+const { configDB } = require("./api/db/configDB.js");
+const { errorHandler } = require("./api/middleware/errorHandler.js");
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+// Connecting DB
+configDB();
+
+app.use("/", userRouter);
+app.use(errorHandler);
+
+port = process.env.PORT;
+
+app.listen(port, console.log(`Server listening at ${port}`));
