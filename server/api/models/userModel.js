b311c365688760c9daa58b99f96ca2acf87271e9
Mohamed Saleem <saleemilahi21@gmail.com>
2022-07-31 15:47:40 +0530

    added password bcrypt

--- server/api/models/userModel.js
+++ server/api/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
 const Joi = require("joi");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -27,10 +28,26 @@ const userSchema = mongoose.Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(this.password, salt);
+
+    this.password = hashPassword;
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 exports.userValidate = Joi.object({
   name: Joi.string().min(3).max(30).trim().required(),
   email: Joi.string().email().lowercase().required(),
   password: Joi.string().min(8).required(),
+  avatar: Joi.string(),
 });
 
 exports.userModel = mongoose.model("user", userSchema);
